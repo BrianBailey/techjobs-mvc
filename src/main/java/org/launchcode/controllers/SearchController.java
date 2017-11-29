90eb88e65c074d6c9c10b02339685cd776f324d8
Brian Bailey <baileybrian12357@gmail.com>
2017-11-29 17:03:18 -0600

    had to make a few changes to make the search all box work properly

--- src/main/java/org/launchcode/controllers/SearchController.java
+++ src/main/java/org/launchcode/controllers/SearchController.java
@@ -5,9 +5,14 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestMethod;
 
+import javax.naming.directory.Attribute;
 import java.util.ArrayList;
 import java.util.HashMap;
+
+
+
 
 /**
  * Created by LaunchCode
@@ -23,5 +28,24 @@ public class SearchController {
     }
 
     // TODO #1 - Create handler to process search request and display results
+    @RequestMapping(value="results")
+    public String search(Model model,
+                         @RequestParam String searchType,
+                         @RequestParam String searchTerm){
+        ArrayList<HashMap<String, String>> jobs;
+        if(searchType.equals("all")){
+            jobs = JobData.findByValue(searchTerm);
+        }
+        else {
+            jobs = JobData.findByColumnAndValue(searchType, searchTerm);
+        }
+
+        model.addAttribute("jobs", jobs);
+        model.addAttribute("columns", ListController.columnChoices);
+        return "search";
+
+    }
+    
 
 }
+
